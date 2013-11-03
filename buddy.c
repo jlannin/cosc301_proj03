df6134244abe3e448f821a9e6432d61bbab7dd57
jlannin <jlannin@colgate.edu>
2013-11-03 12:12:45 -0500

    Updated Project 3
    Here are our updates to Project 3!

--- buddy.c
+++ buddy.c
@@ -12,10 +12,14 @@
 #endif
 
 // function declarations
-void *malloc(size_t);
-void free(void *);
+void *mymalloc(size_t);
+static int checkbuddy(int*, int*);
+void myfree(void *);
 void dump_memory_map(void);
-
+static size_t manageInput(size_t);
+static void *findandalloc(int, int *);
+static int *getbefore(int*);
+static int* merge(int *, int *);
 
 const int HEAPSIZE = (1*1024*1024); // 1 MB
 const int MINIMUM_ALLOC = sizeof(int) * 2;
@@ -23,27 +27,272 @@ const int MINIMUM_ALLOC = sizeof(int) * 2;
 // global file-scope variables for keeping track
 // of the beginning of the heap.
 void *heap_begin = NULL;
+int *freelist = NULL;
 
-
-void *malloc(size_t request_size) {
-
+void *mymalloc(size_t request_size) {
+	if (request_size < 1)
+	{
+		return NULL;
+	}
     // if heap_begin is NULL, then this must be the first
     // time that malloc has been called.  ask for a new
     // heap segment from the OS using mmap and initialize
     // the heap begin pointer.
     if (!heap_begin) {
         heap_begin = mmap(NULL, HEAPSIZE, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
-        atexit(dump_memory_map);
+        freelist = (int *) heap_begin;
+	*(freelist + 0) = HEAPSIZE;
+	*(freelist + 1) = 0;
+	atexit(dump_memory_map);
     }
-
-
+	request_size = manageInput(request_size);  //request_size is size we need
+	int * temp = freelist;
+	if(temp == NULL)
+	{
+		return NULL;
+	}
+	while (*(temp+1) != 0) //next is not 0
+	{
+		if(request_size <= (*(temp+0))) //small enough
+		{
+			return findandalloc(request_size, temp);
+		}
+		else
+		{
+			temp = temp + ((*(temp+1))/4); //next spot in freelist
+		}
+	}
+	if (request_size <= (*(temp+0)))
+	{
+		return findandalloc(request_size, temp);
+	}
+	else
+	{
+		return NULL;
+	}
 }
 
-void free(void *memory_block) {
+static void *findandalloc(int request_size, int *temp)
+{
+	while(request_size <= ((*(temp+0))/2))
+	{
+		int halfsize = (*(temp+0))/2;
+		*(temp + (halfsize/4)) = halfsize; //size of right free block
+		if((*(temp+1))==0)//if in last spot of free
+		{
+			*(temp + (halfsize/4) + 1) = 0;
+		}
+		else
+		{
+			*(temp + (halfsize/4) + 1) = (*(temp+1)) - halfsize; //next of right free block
+		}
+		*(temp+0) = halfsize;
+		*(temp+1) = halfsize;
+	}
+	if(freelist == temp)//freelist is pointing where we are allocating
+	{
+		if( *(freelist+1) == 0)
+		{
+			freelist = NULL;
+		}
+		else
+		{
+			freelist += *(freelist+1)/4;
+		}
+	}
+	else
+	{
+		int * tempfree = freelist;
+		while (tempfree + (*(tempfree + 1)/4) != temp)
+		{
+			tempfree += (*(tempfree + 1)/4);
+		}
+		//at spot before temp
+		if (*(temp+1) == 0) //if block allocating is in last spot
+		{
+			*(tempfree+1) = 0;
+		}
+		else //next = current next + temp's next
+		{
+			*(tempfree+1) += *(temp+1);
+		}
+	}
+	*(temp+1) = 0; //next to 0
+	return (void*)(temp+2); //return after header
+}
 
+static size_t manageInput(size_t size)
+{
+	size_t newsize = size + 8;
+	size_t powtwo = 1;
+	while (powtwo < newsize)
+	{
+		powtwo = powtwo*2;
+	}
+	return powtwo;
+}
+
+void myfree(void *memory_block) 
+{
+	if(memory_block== NULL)
+	{
+		return;
+	}
+	int * memtemp = (int*) memory_block;//header of to be freed block
+	memtemp = (memtemp-2);
+	int * freetemp = freelist;
+
+	if (freelist == NULL)
+	{
+		freelist = memtemp;
+	}
+	else if (freetemp > memtemp) // allocated block is before the start of free list
+	{
+		int dif = (freelist - memtemp)*4;
+		*(memtemp+1) = dif;//set next
+		freelist = memtemp;
+	}
+	else
+	{
+
+		while (((freetemp + (*(freetemp+1))/4)< memtemp) && (*(freetemp + 1) != 0)) //while freelist is before to be freed and not in last spot
+		{
+			freetemp = freetemp + (*(freetemp+1))/4;
+		}
+		//should be at free block before our to be freed block
+		if (*(freetemp+1)!= 0)
+		{
+			int dif = (memtemp - freetemp)*4;
+			*(memtemp+1) = *(freetemp+1) - dif;
+			*(freetemp+1) = dif;//set next
+		}
+		else//last of freelist
+		{
+			int dif = (memtemp - freetemp)*4;
+			*(freetemp+1) = dif;//set next
+		}
+		
+	}
+	while(1)
+	{
+		int *before = getbefore(memtemp);
+		if(checkbuddy(before, memtemp)) //previous is buddy?
+		{
+			memtemp = merge(before, memtemp);
+		}
+		else if(*(memtemp+0) == *(memtemp+1)) //if size = next, then neighbor in freelist so we can check the right block
+		{
+			if(checkbuddy(memtemp + (*(memtemp+0)/4), memtemp))
+			{
+				memtemp = merge(memtemp, memtemp + (*(memtemp+0)/4));
+			}		
+			else
+			{
+				return;
+			}
+		}
+		else
+		{
+			return;  //no buddies!
+		}
+	}
+}
+
+static int* merge(int *left, int* right)
+{
+	*(right+0) = 0;
+	*(left+0) = (*(left+0))*2;
+	if ((*(right+1)) == 0)
+	{
+		*(left+1) = 0;
+	}
+	else
+	{
+		*(left+1) = *(left+1) + *(right+1);
+	}	
+	*(right+1) = 0;
+	return left;
+}
+
+static int *getbefore(int * memtemp)
+{
+	//freelist == memtemp
+	if(freelist == memtemp)
+	{
+		return memtemp;
+	}
+	int * freetemp = freelist;
+	while (((freetemp + (*(freetemp+1))/4) != memtemp) && (*(freetemp + 1) != 0)) //while freelist is before to be freed and not in last spot
+		{
+			freetemp = freetemp + (*(freetemp+1))/4;
+		}
+	return freetemp;
+}
+
+static int checkbuddy(int* one, int* two)
+{
+	if(*(one) != *(two)) //if the sizes are not the same, no reason to keep going
+	{
+		return 0;
+	}
+	uint64_t convertOne = (uint64_t) one;
+	convertOne = (convertOne - (uint64_t)heap_begin);
+	uint64_t convertTwo = (uint64_t) two;
+	convertTwo = (convertTwo - (uint64_t)heap_begin);
+	uint64_t xor = convertTwo^convertOne;
+	//if xor is power of two, then we know that the two only differ by one bit
+	if (xor == 0)
+	{
+		return 0;
+	}
+	int x = 1;
+	while (x < *(one))
+	{
+	x = x*2;
+	}
+	if(x == xor)//xor is a power of 2
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 void dump_memory_map(void) {
+	int count = 0;
+	int* temp = heap_begin;
+	if(freelist == NULL)
+	{
+		printf("%s%d%s%d%s\n", "Block size(1): ", HEAPSIZE, ", offset ", count, ", allocated");
+		return;
+	}
+	else if(*(temp+1) == 0 && freelist != heap_begin) //1st block allocated
+	{
+		int firstsize = (freelist - (int*) heap_begin)*4;
+		printf("%s%d%s%d%s\n", "Block size(3): ", firstsize, ", offset ", count, ", allocated");
+		count = firstsize;
+	}
+	temp = freelist;
+	while(*(temp+1) != 0) //next is not 0
+	{
+		printf("%s%d%s%d%s\n", "Block size(4): ", *(temp + 0), ", offset ", count, ", free");
+		count = count + *(temp+1);
+		if(*(temp+1) > *(temp+0)) //if next > size
+		{
+			int sizealloc = ((*(temp+1)) - (*(temp + 0))); // next - size
+			printf("%s%d%s%d%s\n", "Block size(5): ", sizealloc, ", offset ", count-sizealloc, ", allocated");
+		}	
+		temp = temp + (*(temp + 1))/4;
+	}
+	printf("%s%d%s%d%s\n", "Block size(6): ", *(temp + 0), ", offset ", count, ", free"); //print out last element of free
+	count = count + *(temp+0);
+	//test if one more allocated
+	if(count != HEAPSIZE)
+	{
+		printf("%s%d%s%d%s\n", "Block size(7): ", HEAPSIZE - count, ", offset ", count, ", allocated");
+	}
 
 }
 
--- main.c
+++ main.c
@@ -4,19 +4,36 @@
 #include <time.h>
 #include <string.h>
 #include <sys/time.h>
-
+void *mymalloc(size_t);
+void myfree(void *);
 void dump_memory_map(void);
 
 int main(int argc, char **argv) {
-    void *m1 = malloc(50);  // should allocate 64 bytes
-    void *m2 = malloc(100); // should allocate 128 bytes
-    free(m1);
-    dump_memory_map();
-    void *m3 = malloc(56);  // should allocate 64 bytes
-    void *m4 = malloc(11);  // should allocate 32 bytes
-    free(m3);
-    void *m5 = malloc(30);  // should allocate 64 bytes
-    void *m6 = malloc(120); // should allocate 128 bytes
-    free(m2);
-    return 0;
+ 
+//myfree(NULL);
+    void *m1 = mymalloc(250000);  // should allocate 64 bytes
+    void *m2 = mymalloc(250000); // should allocate 128 bytes
+    void *m3 = mymalloc(250000);
+ void *m4 = mymalloc(250000);
+    myfree(m1);
+    //myfree(m2);
+     myfree(m3);
+ myfree(m4);
+	void *m6 = mymalloc (2000);
+	void *m5 = mymalloc(300);
+    /*void *m3 = mymalloc(56);  // should allocate 64 bytes
+    //void *m7 = mymalloc(500000);  // should allocate 64 bytes
+    void *m8 = mymalloc(100000);  // should allocate 64 bytes
+   void *m11 = mymalloc(100000);  // should allocate 64 bytes
+   // void *m10 = mymalloc(500000);  // should allocate 64 bytes
+    void *m9 = mymalloc(250000);  // should allocate 64 bytes
+    void *m4 = mymalloc(11);  // should allocate 32 bytes
+    myfree(m3);
+    void *m5 = mymalloc(30);  // should allocate 64 bytes
+    void *m6 = mymalloc(120); // should allocate 128 bytes
+    myfree(m2);
+    //myfree(m7);
+    */
+	return 0;
 }
+
